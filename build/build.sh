dc32ab5979e1ff1871949a7141697fd3ad411b4b
edmund_hu <vhudp@163.com>
2021-11-22 19:40:39 +0800

    930版本，修复已知问题

--- build/build.sh
+++ build/build.sh
@@ -16,19 +16,12 @@ fi
 
 arch=$(arch 2>&1)
 echo "Build Architecture is" "${arch}"
-if [ "${arch:0:5}" = 'aarch' ]; then
-  arch=arm64
-else
-  arch=amd64
-fi
-
-sed -i "s/npu-exporter:.*/npu-exporter:${build_version}/" "${TOP_DIR}"/build/npu-exporter.yaml
 
 OUTPUT_NAME="npu-exporter"
 DOCKER_FILE_NAME="Dockerfile"
 docker_zip_name="npu-exporter-${build_version}-${arch}.tar.gz"
 docker_images_name="npu-exporter:${build_version}"
-function clear_env() {
+function clean() {
   rm -rf "${TOP_DIR}"/output
   mkdir -p "${TOP_DIR}"/output
 }
@@ -38,7 +31,7 @@ function build() {
   CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
   CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
   go build -mod=mod -buildmode=pie -ldflags "-s -extldflags=-Wl,-z,now  -X huawei.com/npu-exporter/collector.BuildName=${OUTPUT_NAME} \
-            -X huawei.com/npu-exporter/collector.BuildVersion=${build_version}" \
+            -X huawei.com/npu-exporter/collector.BuildVersion=${build_version}_linux-${arch}" \
     -o ${OUTPUT_NAME}
   ls ${OUTPUT_NAME}
   if [ $? -ne 0 ]; then
@@ -50,6 +43,7 @@ function build() {
 function mv_file() {
   mv "${TOP_DIR}"/${OUTPUT_NAME} "${TOP_DIR}"/output
   cp "${TOP_DIR}"/build/npu-exporter.yaml "${TOP_DIR}"/output/npu-exporter-"${build_version}".yaml
+  sed -i "s/npu-exporter:.*/npu-exporter:${build_version}/" "${TOP_DIR}"/output/npu-exporter-"${build_version}".yaml
   cp "${TOP_DIR}"/build/${DOCKER_FILE_NAME} "${TOP_DIR}"/output
   chmod 640 "${TOP_DIR}"/output/*
   chmod 500 "${TOP_DIR}"/output/${OUTPUT_NAME}
@@ -57,9 +51,12 @@ function mv_file() {
 }
 
 function main() {
-  clear_env
+  clean
   build
   mv_file
 }
-
+if [ "$1" = clean ]; then
+  clean
+  exit 0
+fi
 main
