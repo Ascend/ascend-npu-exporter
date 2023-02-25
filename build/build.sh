678a5c0c8bded00014cf9272b1451c13e1dc200e
Atlas_kang <goaheada@foxmail.com>
2023-02-25 07:24:29 +0000

    update build/build.sh.

--- build/build.sh
+++ build/build.sh
@@ -44,8 +44,8 @@ function build() {
   cd "${TOP_DIR}/cmd/npu-exporter"
   CGO_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
   CGO_CPPFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -fPIC -ftrapv"
-  go build -mod=mod -buildmode=pie -ldflags "-s -extldflags=-Wl,-z,now  -X huawei.com/npu-exporter/v3/versions.BuildName=${OUTPUT_NAME} \
-            -X huawei.com/npu-exporter/v3/versions.BuildVersion=${build_version}_linux-${arch}" \
+  go build -mod=mod -buildmode=pie -ldflags "-s -extldflags=-Wl,-z,now  -X huawei.com/npu-exporter/v5/versions.BuildName=${OUTPUT_NAME} \
+            -X huawei.com/npu-exporter/v5/versions.BuildVersion=${build_version}_linux-${arch}" \
     -o ${OUTPUT_NAME}
   ls ${OUTPUT_NAME}
   if [ $? -ne 0 ]; then
